87b2d23fa949ced33b863b0f40598cda5cdb7cec
Paul Kristopher Espina <kristopherespina@gmail.com>
2021-03-15 20:07:13 +0800

    ding ding dong

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("asdasd");
+        System.out.println("1");
         System.out.println("asdawdawd");
-        System.out.println(1);
+        System.out.println(2);
     }
 }
