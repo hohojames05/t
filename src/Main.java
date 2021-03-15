400dd0109e2fe1e876f0d0d479d7f767ae2af606
Paul Kristopher Espina <kristopherespina@gmail.com>
2021-03-15 20:07:41 +0800

    dint dong 2

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("asdasd");
-        System.out.println("asdawdawd");
+        System.out.println("2");
         System.out.println(1);
     }
 }
